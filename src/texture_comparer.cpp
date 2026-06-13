dc17eab9edcdee1d0f69b37631898cb5ee36bd8f
Xarblu <xarblu@protonmail.com>
2026-06-13 18:40:14 +0200

    TextureComparer: main comparison logic
    Signed-off-by: Xarblu <xarblu@protonmail.com>

--- src/texture_comparer.cpp
+++ src/texture_comparer.cpp
@@ -3,6 +3,7 @@
 #include <epoxy/gl_generated.h>
 #include <opengl/glshadermanager.h>
 #include <opengl/glshader.h>
+#include <opengl/gltexture.h>
 
 #include <QLoggingCategory>
 #include <QFile>
@@ -10,7 +11,6 @@
 #include <epoxy/gl.h>
 
 #include <memory>
-#include <qloggingcategory.h>
 
 Q_LOGGING_CATEGORY(BBDX_TEXTURE_COMPARER, "kwin_effect_better_blur_dx.texture_comparer", QtInfoMsg)
 
@@ -88,6 +88,9 @@ std::unique_ptr<BBDX::TextureComparer> BBDX::TextureComparer::create() {
         qCDebug(BBDX_TEXTURE_COMPARER) << "Compute shader linking log:\n" << log;
     }
 
+    // store locations of uniform params
+    textureComparer->m_dirtyRectLocation = glGetUniformLocation(textureComparer->m_computeShader, "u_dirtyRect");
+
     // supplementary resources
     glGenBuffers(1, &textureComparer->m_counterBuffer);
 
@@ -114,4 +117,48 @@ BBDX::TextureComparer::~TextureComparer() {
     if (m_glueQuery > 0) {
         glDeleteQueries(1, &m_glueQuery);
     }
+}
+
+void BBDX::TextureComparer::compareAndUpdate(KWin::GLTexture *freshBlit, KWin::GLTexture *cachedBlit, const KWin::Region &localDirtyRegion) const {
+    // bind the textures
+    // TODO: colorspace might be different
+    glBindImageTexture(0, freshBlit->texture(), 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA8);
+    glBindImageTexture(1, cachedBlit->texture(), 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA8);
+
+    // reset and bind counter
+    const GLuint zero = 0;
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_counterBuffer);
+    glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, sizeof(GLuint), &zero);
+    // slot 2 - matching compute shader
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, m_counterBuffer);
+
+    // prepare compute shader
+    glUseProgram(m_computeShader);
+
+    for (const auto &rect : localDirtyRegion.rects()) {
+        // bind dirtyRect
+        glUniform4i(m_dirtyRectLocation, rect.x(), rect.y(), rect.width(), rect.height());
+
+        // dispatch in 16x16 workgroup blocks (ceiled, matching compute shader params)
+        glDispatchCompute((rect.width() + 15) / 16, (rect.height() + 15) / 16, 1);
+    }
+
+    // wait for compute to be done, then fire the query
+    glMemoryBarrier(GL_BUFFER_UPDATE_BARRIER_BIT | GL_COMMAND_BARRIER_BIT);
+
+    KWin::ShaderManager::instance()->pushShader(m_glueShader.get()); 
+
+    // "draw" a single point to check the result of
+    // the compute shader
+    glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+    glDepthMask(GL_FALSE);
+
+    glBeginQuery(GL_ANY_SAMPLES_PASSED, m_glueQuery);
+    glDrawArrays(GL_POINTS, 0, 1);
+    glEndQuery(GL_ANY_SAMPLES_PASSED);
+
+    glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+    glDepthMask(GL_TRUE);
+
+    KWin::ShaderManager::instance()->popShader();
 }
--- src/texture_comparer.hpp
+++ src/texture_comparer.hpp
@@ -2,6 +2,7 @@
 
 #include <opengl/glshadermanager.h>
 #include <opengl/glshader.h>
+#include <opengl/gltexture.h>
 
 #include <epoxy/gl.h>
 
@@ -12,6 +13,7 @@ namespace BBDX {
 class TextureComparer {
     // compute shader - we need to handle this ourselves :p
     GLuint m_computeShader{0};
+    GLint m_dirtyRectLocation{};
 
     // regular vert+frag so let KWin handle it
     std::unique_ptr<KWin::GLShader> m_glueShader{nullptr};
@@ -41,6 +43,22 @@ public:
      */
     TextureComparer(TextureComparer &other) = delete;
     TextureComparer& operator=(TextureComparer &other) = delete;
+
+    /**
+     * Get non-owning copy of the query object ID
+     *
+     * Should only be used to see the result after compareAndUpdate()
+     */
+    GLuint queryObject() const {  return m_glueQuery; }
+
+    /**
+     * Compare and update cachedBlit with freshBlit
+     * within the localDirtyRegion
+     *
+     * The result of the comparison can be found using the
+     * query object returned by queryObject()
+     */
+    void compareAndUpdate(KWin::GLTexture *freshBlit, KWin::GLTexture *cachedBlit, const KWin::Region &localDirtyRegion) const;
 };
 
 }
