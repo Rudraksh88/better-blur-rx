329da4d5918313a5226581f4ffdc14dd25e3e958
Xarblu <xarblu@protonmail.com>
2026-06-20 11:56:04 +0200

    utils: split to seperate impl file to allow logging
    Signed-off-by: Xarblu <xarblu@protonmail.com>

---
+++ src/utils.cpp
@@ -0,0 +1,56 @@
+#include "utils.h"
+
+#include <opengl/gltexture.h>
+#include <opengl/glframebuffer.h>
+
+#include <epoxy/gl.h>
+
+#include <QSize>
+#include <QLoggingCategory>
+#include <qloggingcategory.h>
+
+Q_LOGGING_CATEGORY(BBDX_UTILS, "kwin_effect_better_blur_dx.utils", QtInfoMsg)
+
+QSize BBDX::getTextureSize(const QRect &backgroundRect, const size_t i) {
+    return QSize(std::max(1, backgroundRect.width() / (1 << i)),
+                 std::max(1, backgroundRect.height() / (1 << i)));
+}
+
+void BBDX::setTextureSwizzle(KWin::GLTexture *texture) {
+    texture->setSwizzle(GL_RED, GL_GREEN, GL_BLUE, GL_ONE);
+}
+
+void BBDX::setGLScissor(const KWin::Region &dirtyRegion, const KWin::Rect &backgroundRect) {
+    const auto fbo = KWin::GLFramebuffer::currentFramebuffer();
+    if (!fbo) {
+        qCWarning(BBDX_UTILS) << "BBDX::setGLScissor() called with no GLFramebuffer attached";
+        return;
+    }
+
+    const auto texture = fbo->colorAttachment();
+
+    const double scaleX{static_cast<double>(texture->width()) / static_cast<double>(backgroundRect.width())};
+    const double scaleY{static_cast<double>(texture->height()) / static_cast<double>(backgroundRect.height())};
+
+    // slight expansion to ensure we don't cut of edges
+    const KWin::RectF boundingRect{dirtyRegion.translated(-backgroundRect.topLeft()).boundingRect().adjusted(-8, -8, 8, 8)};
+
+    const double scaledLeft{std::max(0.0, std::floor(boundingRect.left() * scaleX))};
+    const double scaledTop{std::max(0.0, std::floor(boundingRect.top() * scaleY))};
+    const double scaledRight{std::min(static_cast<double>(texture->width()), std::ceil(boundingRect.right() * scaleX))};
+    const double scaledBottom{std::min(static_cast<double>(texture->height()), std::ceil(boundingRect.bottom() * scaleY))};
+
+    const int glWidth{std::max(0, static_cast<int>(scaledRight - scaledLeft))};
+    const int glHeight{std::max(0, static_cast<int>(scaledBottom - scaledTop))};
+
+    const int glX{static_cast<int>(scaledLeft)};
+    const int glY{static_cast<int>(texture->height() - (scaledTop + glHeight))};
+
+    glEnable(GL_SCISSOR_TEST);
+    glScissor(glX, glY, glWidth, glHeight);
+}
+
+void BBDX::clearGLScissor() {
+    glScissor(0, 0, 0, 0);
+    glDisable(GL_SCISSOR_TEST);
+}
--- src/utils.h
+++ src/utils.h
@@ -20,10 +20,7 @@ static const char LOG_PREFIX[]{"better_blur_dx:"};
  * and/or usage of invalid textures to create framebuffers appears to cause performance issues.
  * https://github.com/taj-ny/kwin-effects-forceblur/issues/160
  */
-inline QSize getTextureSize(const QRect &backgroundRect, const size_t i) {
-    return QSize(std::max(1, backgroundRect.width() / (1 << i)),
-                 std::max(1, backgroundRect.height() / (1 << i)));
-}
+QSize getTextureSize(const QRect &backgroundRect, const size_t i);
 
 /**
  * When reading textures make the alpha channel a constant 1.
@@ -35,9 +32,7 @@ inline QSize getTextureSize(const QRect &backgroundRect, const size_t i) {
  * This workaround sort of replaces the artifacts with a dark gradient, which
  * technically isn't correct either but better than looking completely broken.
  */
-inline void setTextureSwizzle(KWin::GLTexture *texture) {
-    texture->setSwizzle(GL_RED, GL_GREEN, GL_BLUE, GL_ONE);
-}
+void setTextureSwizzle(KWin::GLTexture *texture);
 
 /**
  * Enable GL_SCISSOR_TEST and set an appropriate
@@ -46,43 +41,13 @@ inline void setTextureSwizzle(KWin::GLTexture *texture) {
  * implicitly targets the current attached framebuffer and
  * thus must be called after GLFramebuffer::pushFramebuffer()
  */
-inline void setGLScissor(const KWin::Region &dirtyRegion, const KWin::Rect &backgroundRect) {
-    const auto fbo = KWin::GLFramebuffer::currentFramebuffer();
-    if (!fbo) {
-        return;
-    }
-
-    const auto texture = fbo->colorAttachment();
-
-    const double scaleX{static_cast<double>(texture->width()) / static_cast<double>(backgroundRect.width())};
-    const double scaleY{static_cast<double>(texture->height()) / static_cast<double>(backgroundRect.height())};
-
-    // slight expansion to ensure we don't cut of edges
-    const KWin::RectF boundingRect{dirtyRegion.translated(-backgroundRect.topLeft()).boundingRect().adjusted(-8, -8, 8, 8)};
-
-    const double scaledLeft{std::max(0.0, std::floor(boundingRect.left() * scaleX))};
-    const double scaledTop{std::max(0.0, std::floor(boundingRect.top() * scaleY))};
-    const double scaledRight{std::min(static_cast<double>(texture->width()), std::ceil(boundingRect.right() * scaleX))};
-    const double scaledBottom{std::min(static_cast<double>(texture->height()), std::ceil(boundingRect.bottom() * scaleY))};
-
-    const int glWidth{std::max(0, static_cast<int>(scaledRight - scaledLeft))};
-    const int glHeight{std::max(0, static_cast<int>(scaledBottom - scaledTop))};
-
-    const int glX{static_cast<int>(scaledLeft)};
-    const int glY{static_cast<int>(texture->height() - (scaledTop + glHeight))};
-
-    glEnable(GL_SCISSOR_TEST);
-    glScissor(glX, glY, glWidth, glHeight);
-}
+void setGLScissor(const KWin::Region &dirtyRegion, const KWin::Rect &backgroundRect);
 
 /**
  * Cleanup for setGLScissor
  *
  * should be cleared right before drawing on the screen
  */
-inline void clearGLScissor() {
-    glScissor(0, 0, 0, 0);
-    glDisable(GL_SCISSOR_TEST);
-}
+void clearGLScissor();
 
 }
