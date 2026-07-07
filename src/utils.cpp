9d87e6de3e404ad434c972fb7a1a7f8c3e9b8e66
Xarblu <xarblu@protonmail.com>
2026-07-07 11:52:04 +0200

    utils: don't expand boundingRect
    BlurCache::drawToCache() needs this to be exact
    and it should be fine without
    
    Signed-off-by: Xarblu <xarblu@protonmail.com>

--- src/utils.cpp
+++ src/utils.cpp
@@ -34,8 +34,7 @@ void BBDX::setGLScissor(const KWin::Region &dirtyRegion, const KWin::Rect &backg
     const double scaleX{static_cast<double>(texture->width()) / static_cast<double>(backgroundRect.width())};
     const double scaleY{static_cast<double>(texture->height()) / static_cast<double>(backgroundRect.height())};
 
-    // slight expansion to ensure we don't cut of edges
-    const KWin::RectF boundingRect{dirtyRegion.translated(-backgroundRect.topLeft()).boundingRect().adjusted(-8, -8, 8, 8)};
+    const KWin::RectF boundingRect{dirtyRegion.translated(-backgroundRect.topLeft()).boundingRect()};
 
     const double scaledLeft{std::max(0.0, std::floor(boundingRect.left() * scaleX))};
     const double scaledTop{std::max(0.0, std::floor(boundingRect.top() * scaleY))};
